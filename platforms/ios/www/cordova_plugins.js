9870f6d601f3421bd97558d716ca1803f536dc95
Alan Gorton <dev.code1up@googlemail.com>
2014-10-25 16:51:24 +0100

    added plugins and platforms

---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,54 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.ohh2ahh.plugins.appavailability/www/AppAvailability.js",
+        "id": "com.ohh2ahh.plugins.appavailability.AppAvailability",
+        "clobbers": [
+            "appAvailability"
+        ]
+    },
+    {
+        "file": "plugins/nl.x-services.plugins.socialsharing/www/SocialSharing.js",
+        "id": "nl.x-services.plugins.socialsharing.SocialSharing",
+        "clobbers": [
+            "window.plugins.socialsharing"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/Coordinates.js",
+        "id": "org.apache.cordova.geolocation.Coordinates",
+        "clobbers": [
+            "Coordinates"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/PositionError.js",
+        "id": "org.apache.cordova.geolocation.PositionError",
+        "clobbers": [
+            "PositionError"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/Position.js",
+        "id": "org.apache.cordova.geolocation.Position",
+        "clobbers": [
+            "Position"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/geolocation.js",
+        "id": "org.apache.cordova.geolocation.geolocation",
+        "clobbers": [
+            "navigator.geolocation"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.ohh2ahh.plugins.appavailability": "0.3.1",
+    "nl.x-services.plugins.socialsharing": "4.3.8",
+    "org.apache.cordova.geolocation": "0.3.10"
+}
+// BOTTOM OF METADATA
+});
---
+++ platforms/ios/www/js/reportController.js
@@ -0,0 +1,36 @@
+angular.module("starter")
+.controller("ReportCtrl", function ($scope, $cordovaGeolocation, $cordovaSocialSharing) {
+  $scope.geopoint = {
+    latitude: 0,
+    longitude: 0
+  };
+
+  $scope.getLocation = function () {
+    var win = function (position) {
+      $scope.geopoint.latitude  = position.coords.latitude;
+      $scope.geopoint.longitude = position.coords.longitude;
+    };
+
+    var fail = function (err) {
+      $scope.message = "Failed to get location.";
+    };
+
+    $cordovaGeolocation
+      .getCurrentPosition()
+      .then(win, fail);
+  };
+
+  $scope.shareViaTwitter = function () {
+    var win = function (position) {
+      $scope.message = "Shared via Twitter.";
+    };
+
+    var fail = function (err) {
+      $scope.message = "Failed share via Twitter.";
+    };
+
+    $cordovaSocialSharing
+      .shareViaTwitter("Hello" /* , image, link */)
+      .then(win, fail);
+    };
+});
